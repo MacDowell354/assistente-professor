dbd5d23e12f4c7ec0ff105da7420a5899a09b946
MacDowell354 <macdowelldacosta@gmail.com>
2025-06-14 14:46:14 -0300

    Update auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -1,1 +1,16 @@
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+from jose import JWTError, jwt
 
+SECRET_KEY = "segredo-teste"
+ALGORITHM = "HS256"
+
+def get_current_user(request: Request):
+    token = request.cookies.get("token")
+    if not token:
+        raise RedirectResponse("/login")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("sub")
+    except JWTError:
+        raise RedirectResponse("/login")
