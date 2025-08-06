6b4adebada6338082d3d9090420f0277c24cc9a3
MacDowell354 <macdowelldacosta@gmail.com>
2025-08-06 10:59:04 -0300

    Update auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -1,4 +1,4 @@
-from fastapi import Request
+from fastapi import Request, HTTPException, status
 from fastapi.responses import RedirectResponse
 from jose import JWTError, jwt
 
@@ -8,9 +8,9 @@ ALGORITHM = "HS256"
 def get_current_user(request: Request):
     token = request.cookies.get("token")
     if not token:
-        raise RedirectResponse("/login")
+        raise HTTPException(status_code=status.HTTP_303_SEE_OTHER, headers={"Location": "/login"})
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload.get("sub")
     except JWTError:
-        raise RedirectResponse("/login")
+        raise HTTPException(status_code=status.HTTP_303_SEE_OTHER, headers={"Location": "/login"})
